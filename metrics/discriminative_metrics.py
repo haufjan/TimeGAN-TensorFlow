bc3ae8ae484dfbb6385613a80ee690ae6b0f0c58
haufjan <jan_hauf@web.de>
2025-04-05 16:44:27 +0200

    Update discriminative_metrics.py

--- metrics/discriminative_metrics.py
+++ metrics/discriminative_metrics.py
@@ -1,13 +1,10 @@
 import numpy as np
 import tensorflow as tf
-from tensorflow import keras
-import sklearn
+import keras
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 import tqdm
 from tqdm import tqdm
-
-
 
 #Define post-hoc discriminator
 class Discriminator(keras.Model):
@@ -19,12 +16,14 @@ class Discriminator(keras.Model):
                  batch_size: int):
         super().__init__(name='Discriminator')
 
+        # Attributes
         self.seq_len = seq_len
         self.dim = dim
         self.hidden_dim = hidden_dim
         self.epochs = epochs
         self.batch_size = batch_size
 
+        # Layers
         self.rnn = keras.layers.GRU(units=hidden_dim)
         self.rnn.build((None, seq_len, dim))
         self.model = keras.layers.Dense(units=1, activation=None)
@@ -32,7 +31,10 @@ class Discriminator(keras.Model):
         self.activation = keras.layers.Activation('sigmoid')
         self.activation.build((None, 1))
 
+        # Loss function
         self.loss_fn = keras.losses.BinaryCrossentropy(from_logits=True)
+        
+        # Optimizer
         self.optimizer = keras.optimizers.Adam(learning_rate=1e-3)
 
     def call(self, x):
@@ -41,8 +43,9 @@ class Discriminator(keras.Model):
         y_hat = self.activation(y_hat_logit)
         return y_hat_logit, y_hat
 
-def discriminative_score_metrics(original_data: np.ndarray, generated_data: np.ndarray):
-    """Use post-hoc RNN to classify original data and synthetic data
+def discriminative_score_metrics(original_data: np.ndarray, generated_data: np.ndarray) -> float:
+    """
+    Use post-hoc RNN to classify original data and synthetic data.
 
     Args:
         - ori_data: original data
@@ -63,14 +66,14 @@ def discriminative_score_metrics(original_data: np.ndarray, generated_data: np.n
                           epochs=iterations,
                           batch_size=batch_size)
 
-    #Split data into train and test fractions
+    # Split data into train and test fractions
     x_train, x_test, x_hat_train, x_hat_test = train_test_split(original_data, generated_data, test_size=0.2)
 
     ds_train = tf.data.Dataset.from_tensor_slices((x_train, x_hat_train)).cache().shuffle(x_train.shape[0])
 
     ds_test = tf.data.Dataset.from_tensor_slices((x_test, x_hat_test)).cache().shuffle(x_test.shape[0])
 
-    #Define train step
+    # Define train step
     @tf.function
     def train_step(x, x_hat):
         with tf.GradientTape() as tape:
@@ -78,21 +81,21 @@ def discriminative_score_metrics(original_data: np.ndarray, generated_data: np.n
             y_logit_fake, _ = model(x_hat)
 
             d_loss_real = tf.math.reduce_mean(model.loss_fn(tf.ones_like(y_logit_real, dtype=tf.float32),
-                                                            y_logit_real))
+                                                            y_logit_real), axis=0)
             d_loss_fake = tf.math.reduce_mean(model.loss_fn(tf.zeros_like(y_logit_fake, dtype=tf.float32),
-                                                            y_logit_fake))
+                                                            y_logit_fake), axis=0)
             d_loss = d_loss_real + d_loss_fake
         
         grad = tape.gradient(d_loss, model.trainable_variables)
         model.optimizer.apply_gradients(zip(grad, model.trainable_variables))
 
-    #Start training
+    # Start training
     for itt in tqdm(range(model.epochs)):
-        #Mini-batch training
+        # Mini-batch training
         for x, x_hat in ds_train.batch(model.batch_size).prefetch(tf.data.AUTOTUNE):
             train_step(x, x_hat)
     
-    #Define test step
+    # Define test step
     @tf.function
     def test_step(x_test, x_hat_test):
         _, y_pred_real = model(x_test)
