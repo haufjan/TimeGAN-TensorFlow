7291f7a44ba01daeecce97d65b2d2b1de72380f9
haufjan <jan_hauf@web.de>
2025-04-05 16:44:52 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,23 +1,21 @@
 import numpy as np
-import sklearn
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.utils import shuffle
 
-
-
-#Define preprocessing routine
+# Define preprocessing routine
 def preprocessing(*inputs: tuple, sequence_length: int, shuffle_stack: bool = True, random_state: int = None) -> np.ndarray:
-    """Conduct preprocessing: scale data, slice data into sequences and shuffle data stack.
-    Consistent shuffling between multiple data stacks must be performed separetaly."""
+    """
+    Conduct preprocessing: scale data, slice data into sequences and shuffle data stack.
+    """
     return_list = []
     for data, bool_scale in inputs:
-        #Create Minimum-Maximum scaler
+        # Create Minimum-Maximum scaler
         if bool_scale:
             scaler = MinMaxScaler().fit(data)
             data = scaler.transform(data)
             print('\nMaximum values:\n', scaler.data_max_, '\nMinimum values:\n', scaler.data_min_)
 
-        #Create list of sequences from sliding window operation defined by sequence_length and stack to a 3-dimensional array (batch, sequence_length, feature)
+        # Create list of sequences from sliding window operation and stack to a 3-dimensional array (batch, sequence_length, feature)
         data_stack = np.stack([data[i:i+sequence_length] for i in range(len(data) - sequence_length)])
 
         if shuffle_stack:
